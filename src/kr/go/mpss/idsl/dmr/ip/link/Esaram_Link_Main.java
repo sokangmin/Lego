3ec29b48d75dc3271c7205e2e2f0bb985df5e493
Administrator <Administrator@HIKKIN-PC.winitech.com>
2016-05-31 09:52:25 +0900

    add "import "

--- src/kr/go/mpss/idsl/dmr/ip/link/Esaram_Link_Main.java
+++ src/kr/go/mpss/idsl/dmr/ip/link/Esaram_Link_Main.java
@@ -5,6 +5,8 @@ import lombok.extern.log4j.Log4j2;
 import com.wini.lego.flow.Lego;
 import com.wini.lego.flow.MultiThreadLego;
 
+import kr.go.mpss.idsl.dmr.ip.link.esaram.*;
+
 
 @Log4j2
 public class Esaram_Link_Main {
@@ -13,10 +15,10 @@ public class Esaram_Link_Main {
 		log.debug("ㅌㅎㅈㄴ_ㅇㄱ program is starting.");
 		
 		Lego e사람_flow = new MultiThreadLego(1, 30, 0, 1, 60*60*24, true, "e사람_flow");
-		e사람_flow.input("select from webService", kr.go.mpss.idsl.dmr.ip.link.esaram.FromWSBlock.class);
-		e사람_flow.filter("Xml String to Body Object", kr.go.mpss.idsl.dmr.ip.link.esaram.Xml2BodyBlock.class);
-		e사람_flow.filter("Body to List<TFE_TN_HCA_MASTER> Object", kr.go.mpss.idsl.dmr.ip.link.esaram.Body2TFE_TN_HCA_MASTERBlock.class);
-		e사람_flow.output("insert or update to tibero", kr.go.mpss.idsl.dmr.ip.link.esaram.ToDBBlock.class);
+		e사람_flow.input("select from webService", FromWSBlock.class);
+		e사람_flow.filter("Xml String to Body Object", Xml2BodyBlock.class);
+		e사람_flow.filter("Body to List<TFE_TN_HCA_MASTER> Object", Body2TFE_TN_HCA_MASTERBlock.class);
+		e사람_flow.output("insert or update to tibero", ToDBBlock.class);
 		e사람_flow.start();
 	}
 
--- src/kr/go/mpss/idsl/dmr/ip/link/Saeol_Link_Main.java
+++ src/kr/go/mpss/idsl/dmr/ip/link/Saeol_Link_Main.java
@@ -5,6 +5,9 @@ import lombok.extern.log4j.Log4j2;
 import com.wini.lego.flow.Lego;
 import com.wini.lego.flow.MultiThreadLego;
 
+import kr.go.mpss.idsl.dmr.ip.link.saeol_asset.*;
+
+
 
 @Log4j2
 public class Saeol_Link_Main {
@@ -13,12 +16,12 @@ public class Saeol_Link_Main {
 		log.debug("ㅌㅎㅈㄴ_ㅇㄱ program is starting.");
 		
 		Lego 새올_자산_flow = new MultiThreadLego(4, 0, 0, 1, 60*60*24, true, "새올_자산_flow");
-		새올_자산_flow.input("select from directory", kr.go.mpss.idsl.dmr.ip.link.saeol_asset.FromFileBlock.class);
-		새올_자산_flow.filter("Xml File to DRML object",  kr.go.mpss.idsl.dmr.ip.link.saeol_asset.Xml2DRMLBlock.class);	
-		새올_자산_flow.filter("DRML to List<TFE_EQPMN_DAO> object", kr.go.mpss.idsl.dmr.ip.link.saeol_asset.DRML2TFE_ASSETS_INFO_CMMNBlock.class);
-		새올_자산_flow.output("insert or update to tibero", kr.go.mpss.idsl.dmr.ip.link.saeol_asset.ToDBBlock.class);		
-		새올_자산_flow.successListener(kr.go.mpss.idsl.dmr.ip.link.saeol_asset.SuccessBlock.class);
-		새올_자산_flow.failListener(kr.go.mpss.idsl.dmr.ip.link.saeol_asset.FailBlock.class);
+		새올_자산_flow.input("select from directory", FromFileBlock.class);
+		새올_자산_flow.filter("Xml File to DRML object",  Xml2DRMLBlock.class);	
+		새올_자산_flow.filter("DRML to List<TFE_EQPMN_DAO> object", DRML2TFE_ASSETS_INFO_CMMNBlock.class);
+		새올_자산_flow.output("insert or update to tibero", ToDBBlock.class);		
+		새올_자산_flow.successListener(SuccessBlock.class);
+		새올_자산_flow.failListener(FailBlock.class);
 		새올_자산_flow.start();
 	}
 
